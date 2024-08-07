87422436aad0a67160c9090c24ba5251d9b98a34
prada120 <126804973+Prada120@users.noreply.github.com>
2024-08-07 14:26:19 -0300

    js.4

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
+caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-
     {
-        enunciado: "Para o sociólogo Marx Weber,os conflitos sociais seriam resultados das posições assimétricas que os indivíduos ocupam na sociedade.Ela chamou de close, estamento e partido as diferentes esferas. Quais são essas opções?",
-        alternativas: [ 
+      enunciado: "Para o sociologo Max Weber, os conflitos socias seriam resultados das posiçoes assimetricos que os individuos ocupam na sociedades.Ela chamou de close,estamento e partido ás diferentes esferas.Quais essas opçoes vocẽs conhecem?",
+        alternativas: [
             {
                 texto: "Econômica !",
                 afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-                texto: "Socias"
-                afirmação: "Quis saber como usar IA no seu dia a dia."
+                texto: "Socias",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
             },
             {
-                texto: "Política "
-                afirmação: "Quis saber como usar IA no seu dia a dia."
-            },
-        ],   
+                texto: "Política ",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
+            }
+        ]
     },
     {
         enunciado: "O programa auxílio Brasil criado em 2023, é um progroma de transferência de renda que reuniu outros auxílios existentes. Atualmente, o valor médio recebido por família e de R$ 600,00. É correto afirmar que o programa tem qual objetivo?",
         alternativas: [
             {
-                texto:"Reduzir ás taxas de mortalidade intantil"
-                afirmação: " Ao reduzir a taxa, estiver se o risco de um nascido. Vivo morrer antes de chegar a um ano de vida"
+                texto:"Reduzir ás taxas de mortalidade intantil",
+                afirmacao: " Ao reduzir a taxa, estiver se o risco de um nascido. Vivo morrer antes de chegar a um ano de vida"
             },
             {
                 texto: "Garantir o acesso a serviços essenciais",
@@ -68,3 +67,25 @@ function mostraPergunta() {
 }
 
 function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
